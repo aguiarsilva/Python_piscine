6ca319b3ccbbfd6b3819562725e52d034772cdb3
Bruno Aguiar <aguiarsilva@gmail.com>
2026-05-04 13:23:54 +0200

    feat ex04 (python_piscine): Fixed the function to output correctly when error.

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,17 +1,24 @@
 import sys
 
-def checkNum(num: int):
+def main():
+    if len(sys.argv) == 1:
+        return
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+        raise AssertionError("argument is not integer")
+
     if num % 2 == 0:
         print("I'm Even.")
     else:
         print("I'm Odd.")
 
-if len(sys.argv) > 2:
-    raise AssertionError("more than one argument is provided")
-
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    raise AssertionError("argument is not integer")
-
-checkNum(n)
+if __name__ == "__main__":
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+        sys.exit(1)
