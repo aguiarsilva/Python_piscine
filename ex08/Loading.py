32bfdf9cc6c6904fc24df70a9daa747e2f63ee5e
Bruno Aguiar <aguiarsilva@gmail.com>
2026-05-08 09:46:27 +0200

    feat ex08 (python_piscine): added first validation to the function.

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,2 +1,23 @@
+import os
+
+
 def ft_tqdm(lst: range) -> None:
-    pass
+    """
+    This function copies the behaviour and utility of the tdqm function
+    with the yield operator.
+    The output expected must be as close as possible to the original function.
+    """
+    # get total length of the list
+    total = len(lst)
+    if total == 0:
+        return
+
+    # get the terminal get_terminal_size
+    try:
+        cols = os.get_terminal_size().columns
+    except OSError:
+        cols = 80
+
+    bar_width = max(10, cols - 40)
+
+
