c94a8a4d38fe541a6a74db677732b174e6fa2d70
Bruno Aguiar <aguiarsilva@gmail.com>
2026-05-19 12:23:52 +0200

    feat subject_2 (Python_piscine): Organized repo to separate subject by folders. Started ex00 of Subject 2.

---
+++ subject_2/ex00/give_bmi.py
@@ -0,0 +1,16 @@
+def main():
+    
+    def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+        result = []
+        
+        return result
+
+    def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+        result = []
+
+        return result
+
+
+if __name__ == '__main__':
+    main()
+
