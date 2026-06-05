02ea660bc74eeaa28abfa66f0b940fda8d58367e
Bruno leonardo Aguiar silva <baguiar-@c3a4c2.42berlin.de>
2026-06-05 11:53:45 +0200

    feat subject_2/ex05 (python_piscine): added files for ex05 and prototypes for functions

---
+++ subject_2/ex05/pimp_image.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from load_image import ft_load
+
+def ft_invert(array) -> array:
+    pass
+
+def ft_red(array) -> array:
+    pass
+
+def ft_green(array) -> array:
+    pass
+
+def ft_blue(array) -> array:
+    pass
+
+def ft_grey(array) -> array:
+    pass
