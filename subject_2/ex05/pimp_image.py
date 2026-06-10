81de4e023dcb151f76817b23d4d170413c5f3a55
Bruno Aguiar <aguiarsilva@gmail.com>
2026-06-10 08:31:31 +0200

    feat subject_2/ex05 (python_piscine): finished the invert function.

--- subject_2/ex05/pimp_image.py
+++ subject_2/ex05/pimp_image.py
@@ -2,11 +2,35 @@ import cv2
 import numpy as np
 from load_image import ft_load
 
-def ft_invert(array) -> array:
-    pass
+def ft_invert(image: np.ndarray) -> np.ndarray:
+    """
+    Function to invert the image colors
+    """
+    h, w = image.shape[:2]
 
-def ft_red(array) -> array:
-    pass
+    if len(image) == 3:
+        channels = image.shape[2]
+        inverted = np.zeros((h, w, channels), dtype=image.dtype)
+
+        for y in range(h):
+            for x in range(w):
+                for c in range(channels):
+                    original = image[y, x, c]
+                    inverted_value = 255 - original
+                    inverted[y, x, c] = inverted_value
+    else:
+        inverted = np.zeros((h, w), dtype=image.dtype)
+        for y in range(h):
+            for x in range(w):
+                inverted[y, x] = 255 - image[y, x]
+
+    return inverted
+
+def ft_red(image: np.ndarray) -> np.ndarray:
+   """
+   Function to apply a red filter to the image
+   """
+
 
 def ft_green(array) -> array:
     pass
