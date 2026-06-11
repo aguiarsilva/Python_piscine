c4499a28d797bae312f99c7c7169508b6b8b71a7
Bruno leonardo Aguiar silva <baguiar-@c3a4c5.42berlin.de>
2026-06-11 12:07:55 +0200

    feat subject_3/ex00 (python_piscine): started implementation of the function

--- subject_3/ex00/load_csv.py
+++ subject_3/ex00/load_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def load(path: str) -> Dataset:
+def load(path: str) -> pd.DataFrame:
     """
     Function to load the Dataset from a file path
     Args: path where the file is located
@@ -10,4 +10,9 @@ def load(path: str) -> Dataset:
     if path is None:
         raise AssertionError("Error: wrong or inexistent path")
 
+    if not path.endswith('.csv'):
+        raise ValueError(f"Expected .csv file, got {path}")
+
+    df = pd.read_csv(path)
     
+    return df
