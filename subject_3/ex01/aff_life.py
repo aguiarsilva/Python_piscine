838df613234bbbf6f3e304285e9425251826269f
Bruno Aguiar <aguiarsilva@gmail.com>
2026-06-17 12:27:37 +0200

    feat subject_3/ex01 (python_piscine): refactored load function, finished plot function to test for ex01

--- subject_3/ex01/aff_life.py
+++ subject_3/ex01/aff_life.py
@@ -1,9 +1,38 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from load_csv import load
 
 
 def main():
-    df = load(./life_expectancy_years.csv)
+    """
+    Program that loads a dataset, print its dimensions and plot a country's
+    time series. 
+    Returns the dimensions tuple on sucess or None on any error.
+    """
+    try:
+        df = load("./life_expectancy_years.csv")
+        if df is None:
+            return None
+        
+        country = "Germany"
+        if country not in df.index:
+            return None
+
+        years = df.columns.astype(int)
+        values = df.loc[country]
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(years, values, marker='.', linestyle='-')
+        plt.xlabel("Year")
+        plt.ylabel("Life Expectancy")
+        plt.title(f"{country} Life Expectancy Projections")
+        plt.grid(True)
+        plt.show()
+
+        return df.shape
+
+    except Exception as e:
+        return None
+
 
 if __name__ == "__main__":
     main()
--- subject_3/ex01/load_csv.py
+++ subject_3/ex01/load_csv.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 
-def load(path: str) -> pd.DataFrame:
+def load(path: str) -> pd.DataFrame | None:
     """
     Function to load the Dataset from a file path
     Args: path where the file is located
     Writes the dimensions of the dataset and returns it.
     """
     try:
-        df = pd.read_csv(path)
+        df = pd.read_csv(path, index_col=0)
 
         dimensions = df.shape
         print(f"Loading dataset of dimensions ({dimensions[0]} x {dimensions[1]})")
+        return df
 
-        return dimensions
     except Exception:
         return None
         
