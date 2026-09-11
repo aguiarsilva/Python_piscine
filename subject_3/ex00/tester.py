5bc7f986270c8fb40189fc0be71475ce15406de3
Bruno Aguiar <aguiarsilva@gmail.com>
2026-09-11 14:53:13 +0200

    fix (Python_piscine) folders: fixed the folders name to match the subject according to the new Subject Sheet

--- subject_3/ex00/tester.py
+++ subject_3/ex00/tester.py
@@ -1,3 +1,16 @@
-from load_csv import load
+from S1E9 import Charachter, Stark
 
-print(load("life_expectancy_years.csv"))
+Ned = Stark("Ned")
+print(Ned.__dict__)
+print(Ned.is_alive)
+Ned.die()
+print(Ned.is_alive)
+print(Ned.__doc__)
+print(Ned.__init__.__doc__)
+print(Ned.die.__doc__)
+print("---")
+Lyanna = Stark("Lyanna", False)
+print(Lyanna.__dict__)
+
+
+# hodor = Charachter("hodor")
