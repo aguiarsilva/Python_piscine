Python_piscine - subject_1 (ex03): fixed zoom scaling in order to produce the picture as the subject shows. I removed the resizing after cropping the image. If you need a real zoom to make the size bigger, then must reinsert cv2.resize() method after cropping image.

subject_1/ex03/zoom.py
@@ -29,7 +29,7 @@ def zoom_image(image: np.ndarray, y_start: int, y_end: int, x_start: int,
 
     zoomed = cv2.resize(cropped, new_size, interpolation=cv2.INTER_LINEAR)
 
-    return zoomed
+    return cropped
 
 
 def add_axis_scale(image: np.ndarray, step: int = 100) -> np.ndarray: